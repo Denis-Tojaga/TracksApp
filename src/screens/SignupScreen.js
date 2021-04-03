0f26ffd02573ba2fa8e623e42676f580f2427375
Denis <denis.tojaga@edu.fit.ba>
2021-04-03 18:28:21 +0200

    //buttons update

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Button } from "react-native";
 
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
 
-    return <Text style={{ fontSize: 45 }}>SignupScreen!</Text>
+    return <>
+
+        <Text style={{ fontSize: 45 }}>SignupScreen!</Text>
+        <Button title="Go to sign in screen!"
+            onPress={() => navigation.navigate("Signin")}
+        />
+
+
+        <Button title="Go to main flow!"
+            //by pressing go to main flow we want to switch to another navigator
+            //which has trackFlow and 2 more screens
+            onPress={() => navigation.navigate("mainFlow")}
+        />
+
+    </>
+
 };
 
 
