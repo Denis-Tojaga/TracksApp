31bd404cd654e0fefe6adc39758ac3b0a1e819ae
Denis <denis.tojaga@edu.fit.ba>
2021-04-04 12:10:35 +0200

    //editin react-native-elements

---
+++ src/components/Spacer.js
@@ -0,0 +1,29 @@
+//the use of this component is to apply some margin to its child component
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+
+
+
+const Spacer = ({ children }) => {
+
+
+    return (
+        <View style={styles.spacer}>
+            {children}
+        </View>
+    );
+
+};
+
+
+const styles = StyleSheet.create({
+    spacer: {
+        margin: 17
+    }
+
+});
+
+
+export default Spacer;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,26 +1,39 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+//importing pre-build elements with some style, in the docs you can find more about its props
+import { Button, Text, Input } from "react-native-elements";
+//we use spacer component to automatically get some margin around different components
+import Spacer from "../components/Spacer";
 
 
 const SignupScreen = ({ navigation }) => {
 
     return <>
 
-        <Text style={{ fontSize: 45 }}>SignupScreen!</Text>
-        <Button title="Go to sign in screen!"
-            onPress={() => navigation.navigate("Signin")}
-        />
+        <Spacer>
+            <Text h3>Sign Up for Tracker</Text>
+        </Spacer>
 
+        <Input label="Email" />
+        <Spacer />
+        <Input label="Password" />
 
-        <Button title="Go to main flow!"
-            //by pressing go to main flow we want to switch to another navigator
-            //which has trackFlow and 2 more screens
-            onPress={() => navigation.navigate("mainFlow")}
-        />
+        <Spacer>
+            <Button title="Sign Up" />
+        </Spacer>
 
     </>
 
 };
+
+
+//we use navigationOptions to return an object which will change the way stack navigator shows this screen
+SignupScreen.navigationOptions = () => {
+    return {
+        headerShown: false,
+    };
+}
+
 
 
 
