5732e9afb0a565404bfff4b7d54ffa3db2f52a90
Denis <denis.tojaga@edu.fit.ba>
2021-04-09 10:48:55 +0200

    //switching fixed

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { View, StyleSheet } from "react-native";
+import { NavigationEvents } from "react-navigation";
 //we use spacer component to automatically get some margin around different components
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
@@ -20,10 +21,13 @@ const SignupScreen = ({ navigation }) => {
 
 
     //destructurize the signup method so we can use it on button press
-    const { state, signup } = useContext(AuthContext);
+    const { state, signup, clearErrorMessage } = useContext(AuthContext);
 
     return (
         <View style={styles.container}>
+            <NavigationEvents
+                onWillFocus={clearErrorMessage}
+            />
 
             <AuthForm
                 headerText="Sign Up for Tracker"
