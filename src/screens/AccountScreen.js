9d55fe4b2431f3b5bbeea37183dd369fd597d728
Denis <denis.tojaga@edu.fit.ba>
2021-04-10 17:23:47 +0200

    //using SafeAreaView

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { Button } from "react-native-elements";
+//we are importing this to make sure none of the content is showing up behind the status bar of the phone
+import { SafeAreaView } from "react-navigation";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
 
@@ -10,18 +12,15 @@ const AccountScreen = () => {
     const { signout } = useContext(AuthContext);
 
     return (
-        <>
-            <Spacer />
+        <SafeAreaView forceInset={{ top: "always" }} >
             <Text style={{ fontSize: 48 }} >Account Screen</Text>
-
             <Spacer>
                 <Button
                     title="Sign Out"
                     onPress={() => signout()}
                 />
             </Spacer>
-
-        </>
+        </SafeAreaView>
     );
 
 };
