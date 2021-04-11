1749e3c27ac5aeb62cda802985bd82d2387b1b01
Denis <denis.tojaga@edu.fit.ba>
2021-04-11 19:52:37 +0200

    //updating ngrok

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -5,5 +5,5 @@ import axios from "axios";
 //everytime we start ngrok from our machine,we need to change this baseURL
 //just copy the one from forwarding parameter
 export default axios.create({
-    baseURL: "http://9539fa238ad5.ngrok.io"
+    baseURL: "http://d4c007bc3e11.ngrok.io"
 });
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
-
+//in newer version of expo we need to install this maps library
+import { MapView } from "react-native-maps";
 
 const Map = () => {
 
-    return <Text>I am a map!</Text>
+    //mapview is almost the same as image, we need to assign it some height and width
+    return <MapView style={styles.map} />
 };
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        height: 200
+    }
+});
 
 
 export default Map;
