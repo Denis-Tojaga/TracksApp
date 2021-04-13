a58d82b92d9590aa151606735d307c7806972eca
Denis <denis.tojaga@edu.fit.ba>
2021-04-13 11:45:12 +0200

    //map update

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,34 +8,27 @@ import { Context as LocationContext } from "../context/LocationContext";
 
 
 const Map = () => {
-
-    //mapview is almost the same as image, we need to assign it some height and width
-    //on ios it shows apple map, on android google maps
-    //it shows a center of our location
-
-
     //we need the current location from the state
     const { state: { currentLocation } } = useContext(LocationContext);
 
-    //initialRegion says what should we show on the map when it first time shows up
+    //initialRegion prop says what should we show on the map when it first time shows up
+    //region prop we use when we want to track the user's movement
+
 
     //if currentLocation is not null we want to show the map to a user(with longitude and latitude from the currentLocation),
     //currentLocation.coords is where are our longitude and latitude
     //otherwise we will show activity indicator
-
     return (
 
         currentLocation ? <MapView
             style={styles.map}
             initialRegion={{ ...currentLocation.coords, latitudeDelta: 0.01, longitudeDelta: 0.01 }}
+            region={{
+                ...currentLocation.coords, latitudeDelta: 0.01, longitudeDelta: 0.01
+            }}
         >
         </MapView> : <ActivityIndicator size="large" style={{ marginTop: 200 }} />
     );
-
-
-
-
-
 };
 
 
