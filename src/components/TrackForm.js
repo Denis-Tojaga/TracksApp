0e943cae86209b4e21535d2f4ca8c499ea444d1a
Denis <denis.tojaga@edu.fit.ba>
2021-04-15 16:45:27 +0200

    //button styling and config

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,21 +1,80 @@
-import React from "react";
-import { Input, Button } from "react-native-elements";
+import React, { useContext } from "react";
+import { Input, Button, Text } from "react-native-elements";
+import { TouchableOpacity, StyleSheet } from "react-native";
 import Spacer from "./Spacer";
+import { Context as LocationContext } from "../context/LocationContext";
+import { Ionicons } from '@expo/vector-icons';
 
 
 
 
 const TrackForm = () => {
+
+    const { state, startRecording, stopRecording, changeName } = useContext(LocationContext);
+
+
+    const buttonDisplaying = (recordingFlag) => {
+
+        if (recordingFlag) {
+            return <TouchableOpacity style={styles.touchable} onPress={stopRecording}>
+                <Text h4 style={styles.text}>Stop recording</Text>
+                <Ionicons style={styles.icon} name="stop-circle" />
+            </TouchableOpacity>
+
+        } else {
+            return <TouchableOpacity style={styles.touchable} onPress={startRecording}>
+                <Text h4 style={styles.text}>Start recording</Text>
+                <Ionicons style={styles.icon} name="play" />
+            </TouchableOpacity>
+        }
+    };
+
+
+
     return (
         <>
             <Spacer>
-                <Input placeholder="Enter name of the track" />
+                <Input
+                    value={state.name}
+                    onChangeText={changeName}
+                    placeholder="Enter name of the track" />
             </Spacer>
-            <Button title="Start recording" />
+
+            {buttonDisplaying(state.recording)}
+
         </>
     );
 
 };
+
+
+
+
+const styles = StyleSheet.create({
+    touchable: {
+        marginHorizontal: 10,
+        height: 45,
+        borderRadius: 12,
+        backgroundColor: "#2289dc",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 7.5
+    },
+
+    text: {
+        alignSelf: "center",
+        marginLeft: 100,
+        color: "white"
+    },
+
+    icon: {
+        alignSelf: "center",
+        marginRight: 10,
+        fontSize: 30,
+        color: "white"
+    }
+});
+
 
 
 
