b5c9b47d6149e040a6475210046db2f938101a47
Denis <denis.tojaga@edu.fit.ba>
2021-04-15 16:13:16 +0200

    //managing recording

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -5,6 +5,20 @@ const locationReducer = (state, action) => {
     switch (action.type) {
         case "add_current_location":
             return { ...state, currentLocation: action.payload };
+        case "start_recording":
+            return { ...state, recording: true };
+        case "stop_recording":
+            return { ...state, recording: false };
+
+        //here we want to add new location to an array if a user started recording
+        //so we return new state with location prop updated to a new array with added action.payload
+        case "add_location_to_array":
+            return { ...state, locations: [...state.locations, action.payload] };
+
+        case "change_track_name":
+            return { ...state, name: action.payload };
+
+
         default:
             break;
     }
@@ -13,10 +27,26 @@ const locationReducer = (state, action) => {
 
 
 
+const changeName = dispatch = (newName) => {
+    dispatch({ type: "change_track_name", payload: newName });
+};
+
+
+
 
 //actions functions that we will be using from this context
-const startRecording = dispatch => () => { };
-const stopRecording = dispatch => () => { };
+const startRecording = dispatch => () => {
+    dispatch({ type: "start_recording" });
+};
+
+
+
+
+
+
+const stopRecording = dispatch => () => {
+    dispatch({ type: "stop_recording" });
+};
 
 
 
@@ -25,8 +55,13 @@ const stopRecording = dispatch => () => { };
 
 //updating the current location property
 //takes a new location and sends it as a payload to reducer function
-const addLocation = dispatch => (newLocation) => {
+const addLocation = dispatch => (newLocation, recording) => {
+
     dispatch({ type: "add_current_location", payload: newLocation });
+
+    if (recording) {
+        dispatch({ type: "add_location_to_array", payload: newLocation });
+    }
 };
 
 
@@ -39,6 +74,6 @@ const addLocation = dispatch => (newLocation) => {
 //state object with all its props
 export const { Context, Provider } = createDataContext(
     locationReducer,
-    { startRecording, stopRecording, addLocation },
-    { recording: false, locations: [], currentLocation: null }
+    { startRecording, stopRecording, addLocation, changeName },
+    { name: "", recording: false, locations: [], currentLocation: null }
 );
