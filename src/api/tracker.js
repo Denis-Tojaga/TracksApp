b7389581a930464dc123b9b91c90a320eb4b18ba
Denis <denis.tojaga@edu.fit.ba>
2021-04-17 16:55:31 +0200

    //ngrok update

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -5,5 +5,5 @@ import axios from "axios";
 //everytime we start ngrok from our machine,we need to change this baseURL
 //just copy the one from forwarding parameter
 export default axios.create({
-    baseURL: "http://c8978f573858.ngrok.io"
+    baseURL: "http://652f6f9f8cc2.ngrok.io"
 });
