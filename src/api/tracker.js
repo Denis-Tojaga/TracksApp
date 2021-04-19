8c731f55f4e48dd9bf68ac3f3331bc971e938f62
Denis <denis.tojaga@edu.fit.ba>
2021-04-19 17:47:35 +0200

    //fetch finished

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -4,8 +4,6 @@ import AsyncStorage from "@react-native-community/async-storage";
 
 //everytime we start ngrok from our machine,we need to change this baseURL
 //just copy the one from forwarding parameter
-//this runs before we make a request
-//we can add in functions 
 const instance = axios.create({
     baseURL: "http://7d9b713db0d6.ngrok.io"
 });
@@ -16,18 +14,15 @@ const instance = axios.create({
 // ---|| -- anytime there is an error with our request
 
 
-
 //config object has some information about the request we are gonna made
 instance.interceptors.request.use(
 
     async (config) => {
 
         const token = await AsyncStorage.getItem("token");
-
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
-
         return config;
     },
 
@@ -37,6 +32,9 @@ instance.interceptors.request.use(
         return Promise.reject(err);
     }
 );
+
+
+
 
 
 export default instance;
