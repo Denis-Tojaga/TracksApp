0657f96b20334a49b9ae07d5b9ef011a452f0722
Denis <denis.tojaga@edu.fit.ba>
2021-04-19 18:00:22 +0200

    //displaying a list of content

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,14 +1,44 @@
-import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import React, { useContext } from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { ListItem } from "react-native-elements";
+import { NavigationEvents } from "react-navigation";
+import { Context as TrackContext } from "../context/TrackContext";
+import Spacer from "../components/Spacer";
+
 
 
 const TrackListScreen = ({ navigation }) => {
 
+    const { state, fetchTracks } = useContext(TrackContext);
+
     return <>
+        <NavigationEvents onWillFocus={() => fetchTracks()} />
         <Text style={{ fontSize: 45 }}>TrackListScreen!</Text>
-        <Button title="Go to track detail!"
-            onPress={() => navigation.navigate("TrackDetail")}
+        <Spacer />
+
+        <FlatList
+            data={state}
+
+            keyExtractor={(item) => item._id}
+
+            renderItem={({ item }) => {
+                return (
+                    <TouchableOpacity>
+                        <ListItem>
+                            <ListItem.Content>
+                                <ListItem.Title>{item.name}</ListItem.Title>
+                            </ListItem.Content>
+                            <ListItem.Chevron />
+                        </ListItem>
+                    </TouchableOpacity>
+                );
+            }}
         />
+
+
+
+
+
     </>
 };
 
