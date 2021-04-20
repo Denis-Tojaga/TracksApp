ed22a845379a12525a73e8ae9e44987d15ab4cd8
Denis <denis.tojaga@edu.fit.ba>
2021-04-20 11:02:18 +0200

    //displaying clicked  track

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { Context as TrackContext } from "../context/TrackContext";
 
 
 const TrackDetailScreen = ({ navigation }) => {
 
+    //importing state so we can access all tracks
+    const { state } = useContext(TrackContext);
+
+    //getting the ID of the track we clicked on
     const _id = navigation.getParam("_id");
 
+    //find function iterates withing state, returns bool value if condition is true 
+    const currentTrack = state.find(t => t._id == _id);
 
-    console.log(_id);
-
-    return <Text style={{ fontSize: 45 }}>TrackDetailScreen!</Text>
+    return <Text style={{ fontSize: 45 }}>{currentTrack.name}</Text>
 };
 
 
